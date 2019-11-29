5780f2caaeae0268bf899feb40d94f471610b016
lopexgt <58342834+lopexgt@users.noreply.github.com>
2019-11-29 14:58:53 -0600

    Update enviar.php

--- enviar.php
+++ enviar.php
@@ -1,30 +1,22 @@
-<?php
-if(isset($_POST['email'])) {
+<?php  
 
-// Debes editar las próximas dos líneas de código de acuerdo con tus preferencias
-$email_to = "marlyn89.go@gmail.com";
-$email_subject = "Contacto desde el sitio web";
+// Llamando a los campos
+$nombre = $_POST['nombre'];
+$correo = $_POST['email'];
+$telefono = $_POST['telefono'];
+$mensaje = $_POST['mensaje'];
 
-// Aquí se deberían validar los datos ingresados por el usuario
-if(!isset($_POST['nombre']) ||
-!isset($_POST['email']) ||
-!isset($_POST['telefono']) ||
-!isset($_POST['mensaje'])) {
+// Datos para el correo
+$destinatario = "marlyn89.go@gmail.com";
+$asunto = "Contacto desde nuestra web";
 
+$carta = "De: $nombre \n";
+$carta .= "Correo: $email \n";
+$carta .= "Telefono: $telefono \n";
+$carta .= "Mensaje: $mensaje";
 
-$email_message = "Detalles del formulario de contacto:\n\n";
-$email_message .= "Nombre: " . $_POST['nombre'] . "\n";
-$email_message .= "E-mail: " . $_POST['email'] . "\n";
-$email_message .= "Teléfono: " . $_POST['telefono'] . "\n";
-$email_message .= "Comentarios: " . $_POST['mensaje'] . "\n\n";
+// Enviando Mensaje
+mail($destinatario, $asunto, $carta);
+header('Location:mensaje-de-envio.html');
 
-
-// Ahora se envía el e-mail usando la función mail() de PHP
-$headers = 'From: '.$email_from."\r\n".
-'Reply-To: '.$email_from."\r\n" .
-'X-Mailer: PHP/' . phpversion();
-@mail($email_to, $email_subject, $email_message, $headers);
-
-echo "¡El formulario se ha enviado con éxito!";
-}
 ?>
